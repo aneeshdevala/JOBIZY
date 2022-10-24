b7ec7b4c235fd274befe7ebaa13d5e969b912a61
aneesh <aneesh172.ant@gmail.com>
2022-10-24 18:56:34 +0530

    Job created done

--- lib/app/services/jobservice.dart
+++ lib/app/services/jobservice.dart
@@ -8,33 +8,6 @@ import 'package:jobizy/app/services/exceptionhandling.dart';
 import 'package:jobizy/app/services/interceptor.dart';
 import 'package:jobizy/app/util/url.dart';
 
-class GetAllJobsService {
-  Future<Postedjobsresponse?> getAlljobs(context) async {
-    Dio dios = await Interceptorapi().getApiUser();
-
-    try {
-      final Response response = await dios.get(Url().postedjobs);
-      if (response.statusCode! >= 200 || response.statusCode! <= 299) {
-        final dataList = (response.data as List).map((e) {
-          print(e.toString());
-          return Postedjobsmodel.fromJson(e);
-        }).toList();
-        return Postedjobsresponse(success: dataList.isEmpty);
-      } else {
-        return Postedjobsresponse(
-            message: 'Something went wrong ! Please try again later');
-      }
-    } on DioError catch (e) {
-      final errormessage = DioException.fromDioError(e).toString();
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(errormessage)));
-    } catch (e) {
-      return Postedjobsresponse(status: 401, message: 'Something went wrong');
-    }
-    return null;
-  }
-}
-
 class GetAll {
   Future<List<Postedjobsmodel>?> getAlljobsFor(context) async {
     Dio dios = await Interceptorapi().getApiUser();
