48ba3208115202cb4147fe7cfa679c5dde49ce6c
aneesh <aneesh172.ant@gmail.com>
2022-11-26 17:09:54 +0530

    applied jobs get succeses

---
+++ lib/app/services/homeservices/getallpost.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:jobizy/app/module/homescreen/model/getpost.dart';
+import 'package:jobizy/app/services/exceptionhandling.dart';
+import 'package:jobizy/app/services/interceptor.dart';
+import 'package:jobizy/app/util/url.dart';
+
+class GetPostService{
+  Future<List<Getpostmodel>?> getAppliedJobs(context) async {
+    Dio dios = await Interceptorapi().getApiUser();
+
+    try {
+      final Response response = await dios.get(Url().getpost);
+      if (response.statusCode! >= 200 || response.statusCode! <= 299) {
+        final postList = (response.data as List)
+            .map((e) => Getpostmodel.fromJson(e))
+            .toList();
+        return postList;
+      } else {
+        const ScaffoldMessenger(
+            child: Text('Something went wrong ! Please try again'));
+        return [];
+      }
+    } on DioError catch (e) {
+      final errormsg = DioException.fromDioError(e).toString();
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(errormsg)));
+    } catch (e) {
+      return [];
+    }
+    return null;
+  }
+}
--- lib/app/util/providers/allproviders.dart
+++ lib/app/util/providers/allproviders.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:jobizy/app/module/jobsection/appliedjobs/controller/appliedcontroller.dart';
 import 'package:jobizy/app/module/jobsection/getsavedjobs/controller/getsaved.dart';
 import 'package:jobizy/app/module/jobsection/savejobs/controller/savedcontroller.dart';
 import 'package:jobizy/app/module/jobsection/jobapply/controller/applycontroller.dart';
@@ -19,6 +20,7 @@ class AllProviders {
   static Widget providers() {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => ApliedController(context)),
         ChangeNotifierProvider(create: (_) => JobSaveController()),
         ChangeNotifierProvider(
             create: (context) => SavedJobsController(context)),
